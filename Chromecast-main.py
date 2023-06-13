232bdf9b25934f16b2c3dbb1dca73693b71e5ad6
Hidden-Warden <70717127+Hidden-Warden@users.noreply.github.com>
2023-06-13 09:45:29 +0200

    v1.2

--- Chromecast-main.py
+++ Chromecast-main.py
@@ -6,6 +6,7 @@ import tkinter as tk
 from threading import Thread
 
 global cast
+global mc
 global NameOfCast
 global TempsDePause
 global Medias
@@ -34,38 +35,40 @@ def start_cast(stop_connection = False):
         except:
             pass
 
-
 # Share an image (from a URL) to the Chromecast
-def show_media(url):
+def show_media(url, temp=10):
     global cast
+    global mc
     mc = cast.media_controller
     #si url contient "http" alors c'est une image
     if "jpg" in url:
         mc.play_media(url, 'image/jpeg')
+        mc.block_until_active()
+    #if mp4, play video and wait for it to finish (get time from CSV file)
+    elif "mp4" in url:
+        mc.play_media(url, 'video/mp4')
+        mc.block_until_active()
+        time.sleep(int(Medias[temp][1]))
+
     else:
         mc.play_media(url, 'image/jpeg')
-    #
-    mc.block_until_active()
+        mc.block_until_active()
     return mc.status
-# Stop the media playing on the Chromecast and disconnect
-"""
-def stop_cast():
-    global cast
-    cast.quit_app()
-    cast.disconnect()
-"""
+
 # Loop through the images in the list (url(s) inside of csv file)
 def loop(number, Repetition=1, EndAfterLoop = False):
     for i in range(Repetition):
         for x in range(number):
-            show_media(Medias[x][0])
+            show_media(Medias[x][0], x)
+            
             time.sleep(TempsDePause)
     if EndAfterLoop == True:
         start_cast(stop_connection = True)
 
-####
-#UI
-####
+def clear_log(): # Clear the log file
+        with open("log.txt", "w") as file:
+            file.write("")
+##### UI ####
 def UI():
     def Send():
         global NameOfCast
@@ -75,6 +78,14 @@ def UI():
         Repetition = int(text3.get())
         print("NameOfCast:",NameOfCast)
         print("TempsDePause:",TempsDePause)
+        
+        # Save the inputs to a text file
+        with open("log.txt", "w") as file:
+            file.write(f"Name of Chromecast: {NameOfCast}\n")
+            file.write(f"Time between photos (s): {TempsDePause}\n")
+            file.write(f"Repetition: {Repetition}\n")
+            file.write(f"End after loop: {checkbox_var.get()}\n")
+
         thread = Thread(target=start_and_loop, args=(Repetition,))
         thread.start()
 
@@ -85,6 +96,12 @@ def UI():
         else:
             loop(len(Medias), Repetition)    
 
+    def quit():
+        print("Quit")
+        start_cast(stop_connection = True)
+        time.sleep(1)
+        root.destroy()
+
     # create UI
     root = tk.Tk()
     root.title("Chromecast-URL")
@@ -92,19 +109,46 @@ def UI():
     root.resizable(False, False)
     root.configure(background='#2B2B2B')
         
-    #text 
-    text1_label = tk.Label(root, text="Name of Chromecast:")
-    text1_label.pack()
-    text1 = tk.Entry(root)
-    text1.pack()
-    text2_label = tk.Label(root, text="Time between photos (s):")
-    text2_label.pack()
-    text2 = tk.Entry(root)
-    text2.pack()
-    text3_label = tk.Label(root, text="Repetition:")
-    text3_label.pack()
-    text3 = tk.Entry(root)
-    text3.pack()
+    # Load previous input values from file
+    try:
+        with open("log.txt", "r") as file:
+            lines = file.readlines()
+            # Parse the lines to get the input values
+            name_of_chromecast = lines[0].split(":")[1].strip()
+            time_between_photos = lines[1].split(":")[1].strip()
+            repetition = lines[2].split(":")[1].strip()
+            end_after_loop = lines[3].split(":")[1].strip()
+            
+            # Set the values in the input fields
+            text1_label = tk.Label(root, text="Name of Chromecast:")
+            text1_label.pack()
+            text1 = tk.Entry(root)
+            text1.insert(0, name_of_chromecast)
+            text1.pack()
+            text2_label = tk.Label(root, text="Time between photos (s):")
+            text2_label.pack()
+            text2 = tk.Entry(root)
+            text2.insert(0, time_between_photos)
+            text2.pack()
+            text3_label = tk.Label(root, text="Repetition:")
+            text3_label.pack()
+            text3 = tk.Entry(root)
+            text3.insert(0, repetition)
+            text3.pack()
+      
+    except:
+        text1_label = tk.Label(root, text="Name of Chromecast:")
+        text1_label.pack()
+        text1 = tk.Entry(root)
+        text1.pack()
+        text2_label = tk.Label(root, text="Time between photos (s):")
+        text2_label.pack()
+        text2 = tk.Entry(root)
+        text2.pack()
+        text3_label = tk.Label(root, text="Repetition:")
+        text3_label.pack()
+        text3 = tk.Entry(root)
+        text3.pack()
 
     #create another checkbox
     checkbox_var = tk.IntVar(value=1)
@@ -115,16 +159,14 @@ def UI():
     submit_button = tk.Button(root, text="Submit", command=Send)
     submit_button.pack()
 
-    def quit():
-        print("Quit")
-        start_cast(stop_connection = True)
-        time.sleep(1)
-        root.destroy()
-
     # Create the quit button
     quit_button = tk.Button(root, text="Quit and end connection", command=quit)
     quit_button.pack()
 
+    # Create the clear log button
+    clear_log_button = tk.Button(root, text="Clear log", command=clear_log)
+    clear_log_button.pack()
+    
     # When the window is closed, quit the application
     root.protocol("WM_DELETE_WINDOW", quit)
 
